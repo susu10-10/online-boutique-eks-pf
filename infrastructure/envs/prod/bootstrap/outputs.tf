85003f30b62a4b8dd22e65f39c06e573992ce541
susu10-10 <successchukwu20@gmail.com>
2026-08-28 22:52:45 +0200

    style: format output values for consistency in outputs.tf

--- infrastructure/envs/prod/bootstrap/outputs.tf
+++ infrastructure/envs/prod/bootstrap/outputs.tf
@@ -1,16 +1,16 @@
 output "cluster_name" {
-description = "EKS Cluster name"
+  description = "EKS Cluster name"
   value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-description = "EKS Cluster endpoint"
+  description = "EKS Cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
 
 output "acm_certificate_arn" {
   description = "The ARN of the validated ACM certificate"
-  value = module.acm.acm_certificate_arn
+  value       = module.acm.acm_certificate_arn
 }
 
 
@@ -21,25 +21,25 @@ output "cluster_certificate_authority_data" {
 
 output "ebs_csi_driver_irsa_role_arn" {
   description = "IAM Role ARN for the EBS CSI Driver"
-  value = module.ebs_csi_driver_irsa.iam_role_arn
+  value       = module.ebs_csi_driver_irsa.iam_role_arn
 }
 
 output "aws_lb_controller_irsa_role_arn" {
   description = "IAM Role ARN for the AWS Load Balancer Controller"
-  value = module.alb_irsa.iam_role_arn
+  value       = module.alb_irsa.iam_role_arn
 }
 
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "private_subnet" {
   description = "The IDs of the private subnets"
-  value = module.vpc.private_subnet_ids
+  value       = module.vpc.private_subnet_ids
 }
 
 output "oidc_provider_arn" {
   description = "The ARN of the EKS OIDC Provider"
-  value = module.eks.oidc_provider_arn
+  value       = module.eks.oidc_provider_arn
 }
