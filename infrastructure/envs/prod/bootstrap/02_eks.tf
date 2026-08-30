f5876bf7d4193a2335088e7ce0ee31570589653c
susu10-10 <successchukwu20@gmail.com>
2026-08-30 22:24:30 +0200

    fix: update min and desired size of EKS managed node group to 2

--- infrastructure/envs/prod/bootstrap/02_eks.tf
+++ infrastructure/envs/prod/bootstrap/02_eks.tf
@@ -38,10 +38,10 @@ module "eks" {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m7i-flex.large"]
-      min_size       = 1
+      min_size       = 2
       max_size       = 3
 
-      desired_size = 1
+      desired_size = 2
     }
   }
 
